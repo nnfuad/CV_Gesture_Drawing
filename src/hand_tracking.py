3a8a34c02ddd635e38d3b65f951b3f4c28a3d975
nnfuad <nnfuad01@gmail.com>
2026-02-07 00:13:58 +0600

    created hand tracking module

---
+++ src/hand_tracking.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+class HandTracker:
+    def __init__(self, detection_conf=0.7, tracking_conf=0.7):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            min_detection_confidence=detection_conf,
+            min_tracking_confidence=tracking_conf
+        )
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def get_landmarks(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.hands.process(frame_rgb)
+
+        if result.multi_hand_landmarks:
+            hand = result.multi_hand_landmarks[0]
+            landmarks = []
+            h, w, _ = frame.shape
+
+            for lm in hand.landmark:
+                landmarks.append((int(lm.x * w), int(lm.y * h)))
+
+            return landmarks
+        return None
